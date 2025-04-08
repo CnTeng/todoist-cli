000109bbb7c29dd36ed4383fc68831f40ac5b51f
CnTeng <rxsnakepi@gmail.com>
2025-04-08 21:47:51 +0800

    feat: read tokens from env

--- internal/daemon/daemon.go
+++ internal/daemon/daemon.go
@@ -2,9 +2,11 @@ package daemon
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"net"
 	"net/http"
+	"os"
 
 	"github.com/CnTeng/todoist-api-go/sync"
 	"github.com/CnTeng/todoist-api-go/ws"
@@ -15,10 +17,15 @@ import (
 	"github.com/creachadair/jrpc2/server"
 )
 
+const (
+	apiTokenEnv     = "API_TOKEN"
+	apiTokenFileEnv = "API_TOKEN_FILE"
+	WsTokenEnv      = "WS_TOKEN"
+	WsTokenFileEnv  = "WS_TOKEN_FILE"
+)
+
 type Config struct {
-	Address  string `toml:"address"`
-	ApiToken string `toml:"api_token"`
-	WsToken  string `toml:"ws_token"`
+	Address string `toml:"address"`
 }
 
 var DefaultConfig = &Config{
@@ -33,16 +40,47 @@ type Daemon struct {
 	log     *log.Logger
 }
 
-func NewDaemon(config *Config, db *db.DB) *Daemon {
+func NewDaemon(db *db.DB, config *Config) *Daemon {
 	d := &Daemon{
 		address: config.Address,
-		client:  sync.NewClient(http.DefaultClient, config.ApiToken, db),
 		db:      db,
 		log:     log.New(log.Writer(), "daemon: ", log.Flags()),
 	}
-	d.ws = ws.NewClient(config.WsToken, d)
 
 	return d
+}
+
+func (d *Daemon) LoadTokens() error {
+	apiToken := os.Getenv(apiTokenEnv)
+	apiTokenFile := os.Getenv(apiTokenFileEnv)
+	if apiToken == "" && apiTokenFile == "" {
+		return fmt.Errorf("%s or %s is required", apiTokenEnv, apiTokenFileEnv)
+	}
+	if apiToken == "" && apiTokenFile != "" {
+		token, err := os.ReadFile(apiTokenFile)
+		if err != nil {
+			return err
+		}
+		apiToken = string(token)
+	}
+
+	wsToken := os.Getenv(WsTokenEnv)
+	wsTokenFile := os.Getenv(WsTokenFileEnv)
+	if wsToken == "" && wsTokenFile == "" {
+		return fmt.Errorf("%s or %s is required,", WsTokenEnv, WsTokenFileEnv)
+	}
+	if wsToken == "" && wsTokenFile != "" {
+		token, err := os.ReadFile(wsTokenFile)
+		if err != nil {
+			return err
+		}
+		wsToken = string(token)
+	}
+
+	d.client = sync.NewClient(http.DefaultClient, apiToken, d.db)
+	d.ws = ws.NewClient(wsToken, d)
+
+	return nil
 }
 
 func (d *Daemon) HandleNotification(ctx context.Context, noti ws.Notification) error {
@@ -69,7 +107,7 @@ func (d *Daemon) Serve(ctx context.Context) error {
 		Sync:          handler.New(d.sync),
 		CompletedGet:  handler.New(d.client.GetCompletedInfo),
 		TaskGet:       handler.New(d.db.GetTask),
-		TaskList:      handler.NewPos(d.db.ListTasks, "all"),
+		TaskList:      handler.NewPos(d.db.ListTasks, "completed"),
 		TaskAdd:       handler.New(d.client.AddItem),
 		TaskQuickAdd:  handler.New(d.client.QuickAddItem),
 		TaskModify:    handler.New(d.client.UpdateItem),
